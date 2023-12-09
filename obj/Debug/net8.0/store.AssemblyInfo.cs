351e21f95c2944b46a23f676921ad53b9105272d
srkreddyg <srkreddyg@outlook.com>
2023-12-09 20:21:20 +0530

    Changed welcome message

--- obj/Debug/net8.0/store.AssemblyInfo.cs
+++ obj/Debug/net8.0/store.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("store")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b47ff296176820eb5489ab84becadd47234ca70")]
 [assembly: System.Reflection.AssemblyProductAttribute("store")]
 [assembly: System.Reflection.AssemblyTitleAttribute("store")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
